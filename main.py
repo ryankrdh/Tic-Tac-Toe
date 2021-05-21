f9e0eefe8d202f9eff6cf001a21650c4c806d9dc
Ryan Kim <ryankrdh@gmail.com>
2021-05-21 14:03:01 -0700

    finished functions for win check and place marker.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+# DISPLAYING THE BOARD
 def display_board(board):
     print('     |     |')
     print('  ' + board[7] + '  |  ' + board[8] + '  |  ' + board[9])
@@ -13,16 +13,18 @@ def display_board(board):
     print('     |     |')
 
 
+# TESTING THE BOARD
 test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
 display_board(test_board)
 
 
+# TAKING PLAYER INPUT AND ASSIGNING X OR O.
 def player_input():
     marker = ''
 
-    while marker != 'X' and marker != 'O':
     # For some reason the following syntax won't provide the user input in pycharm
     # while not (marker == 'X' or marker == 'O'):
+    while marker != 'X' and marker != 'O':
         marker = input('Player 1: Do you want to be X or O? ').upper()
 
     if marker == 'X':
@@ -30,4 +32,33 @@ def player_input():
     else:
         return 'O', 'X'
 
+
 print(player_input())
+
+
+# FUNCTION THAT TAKES IN THE BOARD LIST OBJECT, MARKER, AND POSITION
+def place_marker(board, marker, position):
+
+    board[position] = marker
+
+
+# TESTING THE PLACE MARKER FUNCTION
+place_marker(test_board,'@',4)
+display_board(test_board)
+
+
+# CHECKING FOR WINNING CONDITIONS
+def win_check(board, mark):
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # TOP ROW
+    (board[4] == mark and board[5] == mark and board[6] == mark) or # MIDDLE ROW
+    (board[1] == mark and board[2] == mark and board[3] == mark) or # BOTTOM ROW
+    (board[7] == mark and board[4] == mark and board[1] == mark) or # LEFT COLUMN
+    (board[8] == mark and board[5] == mark and board[2] == mark) or # MIDDLE COLUMN
+    (board[9] == mark and board[6] == mark and board[3] == mark) or # RIGHT COLUMN
+    (board[7] == mark and board[5] == mark and board[3] == mark) or # TOP LEFT TO LOWER RIGHT DIAGONAL
+    (board[9] == mark and board[5] == mark and board[1] == mark)) # TOP RIGHT TO LOWER LEFT DIAGONAL
+
+
+# CHECKING FOR WINNING PLAYER
+display_board(test_board)
+print(win_check(test_board, 'X'))
