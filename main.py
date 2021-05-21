bd28c664f12ab3075b13285244c9964c240be4f7
Ryan Kim <ryankrdh@gmail.com>
2021-05-20 17:41:52 -0700

    creating function for player marker

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+
+
 def display_board(board):
     print('     |     |')
     print('  ' + board[7] + '  |  ' + board[8] + '  |  ' + board[9])
@@ -11,18 +14,19 @@ def display_board(board):
     print('  ' + board[1] + '  |  ' + board[2] + '  |  ' + board[3])
     print('     |     |')
 
-    test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
-    display_board(test_board)
-    print('hi')
+test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+display_board(test_board)
+print('hi')
 
-    def player_input():
-        marker = ''
+def player_input():
+    marker = ''
 
-        while not (marker == 'X' or m
-            marker = input('Player 1: Do you want to be X or O? ').upper()
+    while not (marker == 'X' or 'O'):
+        marker = input('Player 1: Do you want to be X or O? ').upper()
 
-        if marker == 'X':
-            return ('X', 'O')
-        else:
-            return ('O', 'X')
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+
 
