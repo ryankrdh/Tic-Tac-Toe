7ded09f997da79b668f8331f72ebbc1a7d1b6ba7
Ryan Kim <ryankrdh@gmail.com>
2021-05-24 16:45:50 -0700

    * complete re-work and polishing of the whole project + added bot option with MINIMAX *

--- main.py
+++ main.py
@@ -3,268 +3,266 @@ import random
 
 # DISPLAYING THE BOARD
 def display_board(board):
-    print('     |     |')
-    print('  ' + board[7] + '  |  ' + board[8] + '  |  ' + board[9])
-    print('     |     |')
-    print('-----------------')
-    print('     |     |')
-    print('  ' + board[4] + '  |  ' + board[5] + '  |  ' + board[6])
-    print('     |     |')
-    print('-----------------')
-    print('     |     |')
-    print('  ' + board[1] + '  |  ' + board[2] + '  |  ' + board[3])
-    print('     |     |')
+    print("     |     |")
+    print("  " + board[1] + "  |  " + board[2] + "  |  " + board[3])
+    print("     |     |")
+    print("-----------------")
+    print("     |     |")
+    print("  " + board[4] + "  |  " + board[5] + "  |  " + board[6])
+    print("     |     |")
+    print("-----------------")
+    print("     |     |")
+    print("  " + board[7] + "  |  " + board[8] + "  |  " + board[9])
+    print("     |     |")
 
 
-# TESTING THE BOARD
-# test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
-# display_board(test_board)
+# THIS BOARD IS USED FOR POSITIONING
+the_board = {1: " ", 2: " ", 3: " ",
+         4: " ", 5: " ", 6: " ",
+         7: " ", 8: " ", 9: " "}
 
 
-# TAKING PLAYER INPUT AND ASSIGNING X OR O.
+# THIS FUNCTION CHECKS TO SEE IF A PLAYER WON THE GAME
+def check_if_won():
+    if the_board[1] == the_board[2] and the_board[1] == the_board[3] and the_board[1] != " ":
+        return True
+    elif the_board[4] == the_board[5] and the_board[4] == the_board[6] and the_board[4] != " ":
+        return True
+    elif the_board[7] == the_board[8] and the_board[7] == the_board[9] and the_board[7] != " ":
+        return True
+    elif the_board[1] == the_board[4] and the_board[1] == the_board[7] and the_board[1] != " ":
+        return True
+    elif the_board[2] == the_board[5] and the_board[2] == the_board[8] and the_board[2] != " ":
+        return True
+    elif the_board[3] == the_board[6] and the_board[3] == the_board[9] and the_board[3] != " ":
+        return True
+    elif the_board[1] == the_board[5] and the_board[1] == the_board[9] and the_board[1] != " ":
+        return True
+    elif the_board[7] == the_board[5] and the_board[7] == the_board[3] and the_board[7] != " ":
+        return True
+    else:
+        return False
+
+
+# THIS FUNCTION CHECKS TO SEE WHICH MARK WON THE GAME
+def check_which_mark(mark):
+    return ((the_board[7] == mark and the_board[8] == mark and the_board[9] == mark) or  # TOP ROW
+            (the_board[4] == mark and the_board[5] == mark and the_board[6] == mark) or  # MIDDLE ROW
+            (the_board[1] == mark and the_board[2] == mark and the_board[3] == mark) or  # BOTTOM ROW
+            (the_board[7] == mark and the_board[4] == mark and the_board[1] == mark) or  # LEFT COLUMN
+            (the_board[8] == mark and the_board[5] == mark and the_board[2] == mark) or  # MIDDLE COLUMN
+            (the_board[9] == mark and the_board[6] == mark and the_board[3] == mark) or  # RIGHT COLUMN
+            (the_board[7] == mark and the_board[5] == mark and the_board[3] == mark) or  # DIAGONAL
+            (the_board[9] == mark and the_board[5] == mark and the_board[1] == mark))  # DIAGONAL
+
+
+# PLAYER GETS TO DECIDE THE MARKER "O" OR "X"
 def player_input():
-    marker = ''
-    playerCPU = False
+    marker = ""
 
-    # For some reason the following syntax won't provide the user input in pycharm
-    # while not (marker == 'X' or marker == 'O'):
-    while marker != 'X' and marker != 'O':
-        marker = input('Player 1: Do you want to be X or O?').upper()
-        playerCPU = input('Would you like to play with a CPU? yes or no').lower()
-        if playerCPU == 'yes':
-            playerCPU = True
-            print('Player 2 will now be a CPU.')
+    # For some reason the following syntax won"t provide the user input in pycharm
+    # while not (marker == "X" or marker == "O"):
+    while marker != "X" and marker != "O":
+        marker = input("Player 1: Do you want to be X or O?").upper()
+    if marker == "X":
+        return "X", "O"
+    else:
+        return "O", "X"
+
+
+# PERMISSION TO HAVE BOT TAKE CONTROL OF PLAYER 2
+def bot_input():
+    enable_player_bot = False
+
+    while enable_player_bot != "yes" and enable_player_bot != "no":
+        enable_player_bot = input("Would you like to play with a CPU? yes or no").lower()
+        if enable_player_bot == "yes":
+            print(" ** Player 2 will now be a BOT **.")
+            return True
         else:
-            playerCPU = False
-            print('Player 2 will be another player.')
-    if marker == 'X' and playerCPU == True:
-        return ('X', 'O', playerCPU)
+            print(" ** Player 2 will be another player **. ")
+            return False
+
+
+# RANDOM DETERMINATION OF WHICH PLAYER GOES FIRST
+def first_round():
+    coin_toss = random.randint(0, 1)
+
+    if coin_toss == 0:
+        return "Player 1"
     else:
-        return ('O', 'X', playerCPU)
-    if marker == 'X' and playerCPU == False:
-        return ('X', 'O')
+        return "Player 2"
+
+
+# CHECK TO SEE IF THE POSITION ALREADY HAS A MARK
+def space_check(position):
+    if the_board[position] == " ":
+        return True
     else:
-        return ('O', 'X')
+        return False
 
 
-
-
-# print(playerCPU)
-# FUNCTION THAT TAKES IN THE BOARD LIST OBJECT, MARKER, AND POSITION
-def place_marker(board, marker, position):
-    board[position] = marker
-
-
-# TESTING THE PLACE MARKER FUNCTION
-# place_marker(test_board,'@',4)
-# display_board(test_board)
-
-
-# CHECKING FOR WINNING CONDITIONS
-def win_check(board, mark):
-    return ((board[7] == mark and board[8] == mark and board[9] == mark) or  # TOP ROW
-            (board[4] == mark and board[5] == mark and board[6] == mark) or  # MIDDLE ROW
-            (board[1] == mark and board[2] == mark and board[3] == mark) or  # BOTTOM ROW
-            (board[7] == mark and board[4] == mark and board[1] == mark) or  # LEFT COLUMN
-            (board[8] == mark and board[5] == mark and board[2] == mark) or  # MIDDLE COLUMN
-            (board[9] == mark and board[6] == mark and board[3] == mark) or  # RIGHT COLUMN
-            (board[7] == mark and board[5] == mark and board[3] == mark) or  # TOP LEFT TO LOWER RIGHT DIAGONAL
-            (board[9] == mark and board[5] == mark and board[1] == mark))  # TOP RIGHT TO LOWER LEFT DIAGONAL
-
-
-# CHECKING FOR WINNING PLAYER
-# display_board(test_board)
-# print(win_check(test_board, 'X'))
-
-
-# FUNCTION THAT DETERMINES WHICH PLAYER GOES FIRST
-def choose_first():
-    flip = random.randint(0, 1)
-
-    if flip == 0:
-        return 'Player 1'
+# ENTER MARK OF THE PLAYER IN THE POSITION AND CHECK FOR WIN
+def player_position_choice(letter, position):
+    if space_check(position):
+        the_board[position] = letter
+        display_board(the_board)
+        if check_if_draw():
+            print(" ** DRAW! **")
+            print("Thanks for playing!")
+            exit()
+        if check_if_won():
+            if letter == player2_marker:
+                print(" ** Player 2 WINS! ** ")
+                print("Thanks for playing!")
+                exit()
+            else:
+                print(" ** Player 1 WINS! ** ")
+                print("Thanks for playing!")
+                exit()
+        return
     else:
-        return 'Player 2'
+        print("That space is full!")
+        position = int(input("Please enter a new position:  "))
+        player_position_choice(letter, position)
+        return
 
 
-# CHECK TO SEE IF SPACE IS AVAILABLE
-def space_check(board, position):
-    return board[position] == ' '
-
-
-# CHECK TO SEE IF BOARD IS FULL
-def full_board_check(board):
-    for i in range(1, 10):
-        if space_check(board, i):
+# CHECKS TO SEE IF THERE IS A DRAW
+def check_if_draw():
+    for key in the_board.keys():
+        if the_board[key] == " ":
             return False
     return True
 
 
-# IF THE SPACE IS FREE, INPUT MARKER
-def player_choice(board):
-    position = 0
-
-    while position not in range(1, 10) or not space_check(board, position):
-        position = int(input('Choose a position: (1-9) '))
-
-    return position
-
-# CPU strategy
-def cpu_choice(board):
-    position = 0
-
-    while position not in range(1, 10) or not space_check(board, position):
-        position = 1 or 3 or 7 or 9
-
-    return position
-
-
 # ASKING THE PLAYER IF THEY WANT TO PLAY AGAIN
 def replay():
-    choice = ''
+    choice = ""
 
-    while choice != 'YES' and choice != 'NO':
-        choice = input('Play again? Enter YES or NO').upper()
+    while choice != "yes" and choice != "no":
+        choice = input("Play again? Enter YES or NO").lower()
 
-        print('\n' * 100)
-    return choice == 'YES'
+        print("\n" * 3)
+    return choice == "YES"
 
 
-print('Welcome to Tic Tac Toe!')
+# ASKING PLAYER ONE TO MOVE
+def player_one_move():
+    position = 0
 
-while True:
+    while position not in range(1, 10):
+        position = int(input("Enter the position for " + player1_marker + ": "))
+    player_position_choice(player1_marker, position)
+    print("\n" * 2)
+    return
 
-    # PLAY THE GAME
 
-    # SET EVERYTHING UP (BOARD, WHO GOES FIRST, CHOOSE MARKERS)
-    the_board = [' '] * 10
-    player1_marker, player2_marker, cpu_marker = player_input()
+# ASKING PLAYER TWO TO MOVE
+def player_two_move():
+    position = 0
 
-    turn = choose_first()
-    print(turn + ' will go first')
+    while position not in range(1, 10):
+        position = int(input("Enter the position for " + player2_marker + ": "))
+    player_position_choice(player2_marker, position)
+    print("\n" * 2)
+    return
 
-    play_game = input('Ready to play? yes or no?').lower()
 
-    if play_game == 'yes':
-        game_on = True
+# MINIMAX AI BOT FOR PLAYER TWO
+def player_two_bot():
+    high_score = -800
+    best_position = 0
+    for key in the_board.keys():
+        if the_board[key] == " ":
+            the_board[key] = player2_marker
+            score = minimax(the_board, 0, False)
+            the_board[key] = " "
+            if score > high_score:
+                high_score = score
+                best_position = key
+
+    player_position_choice(player2_marker, best_position)
+    print("\n" * 2)
+    return
+
+
+# MINIMAX AI BOT FOR PLAYER TWO
+def minimax(board, depth, maximizing):
+    if check_which_mark(player2_marker):
+        return 1
+    elif check_which_mark(player1_marker):
+        return -1
+    elif check_if_draw():
+        return 0
+
+    if maximizing:
+        high_score = -800
+        for key in the_board.keys():
+            if the_board[key] == " ":
+                the_board[key] = player2_marker
+                score = minimax(board, depth + 1, False)
+                the_board[key] = " "
+                if score > high_score:
+                    high_score = score
+        return high_score
+
     else:
-        game_on = False
+        high_score = 800
+        for key in the_board.keys():
+            if the_board[key] == " ":
+                the_board[key] = player1_marker
+                score = minimax(board, depth + 1, True)
+                the_board[key] = " "
+                if score < high_score:
+                    high_score = score
+        return high_score
 
-    if cpu_marker:
-        print('You are playing against a CPU. The CPU has a ' + player2_marker + ' marker')
 
-    ## GAMEPLAY WITH CPU
-    while cpu_marker and game_on:
+# GAME INSTRUCTIONS
+print("The board positions go from 1-9 from the top:")
+print("     |     |")
+print("  1  |  2  |  3")
+print("     |     |")
+print("-----------------")
+print("     |     |")
+print("  4  |  5  |  6")
+print("     |     |")
+print("-----------------")
+print("     |     |")
+print("  7  |  8  |  9")
+print("     |     |")
+print("\n")
 
-        ### PLAYER ONE TURN
-        if turn == 'Player 2':
 
-            # Show the board
-            display_board(the_board)
+# DETERMINES WHICH PLAYER GOES FIRST, WHICH MARK, AND BOT
+turn = first_round()
+print("First turn will be randomized.")
+print(turn + " will be going first this game!")
+player1_marker, player2_marker = player_input()
+bot = bot_input()
 
-            # Choose a position
-            position = player_choice(the_board)
-            # Place the marker on the position
-            place_marker(the_board, player2_marker, position)
-            # Check if they won
-            if win_check(the_board, player2_marker):
-                print('\n' * 100)
-                display_board(the_board)
-                print('CPU has won')
-                game_on = False
-            else:
-                # Or check if there is a tie
-                if full_board_check(the_board):
-                    display_board(the_board)
-                    print('Tie game')
-                    game_on = False
-                else:
-                    # No tie and no Win? then next player's turn
-                    turn = 'Player 1'
-                    print('\n' * 100)
 
-        ### PLAYER ONE TURN
+# STARTS THE GAME
+while not check_if_won():
+    while bot:
+        if turn == "Player 1":
+            print("Player 1 TURN")
+            player_one_move()
+            print("Player 2 TURN")
+            player_two_bot()
         else:
-            # Show the board
-            display_board(the_board)
-
-            # Choose a position
-            position = player_choice(the_board)
-            # Place the marker on the position
-            place_marker(the_board, player1_marker, position)
-            # Check if they won
-            if win_check(the_board, player1_marker):
-                print('\n' * 100)
-                display_board(the_board)
-                print('Player 1 has won')
-                game_on = False
-            else:
-                # Or check if there is a tie
-                if full_board_check(the_board):
-                    display_board(the_board)
-                    print('Tie game')
-                    game_on = False
-                else:
-                    # No tie and no Win? then next player's turn
-                    turn = 'Player 2'
-                    print('\n' * 100)
-
-    ## GAMEPLAY
-    while game_on and not cpu_marker:
-
-        ### PLAYER ONE TURN
-        if turn == 'Player 1':
-
-            # Show the board
-            display_board(the_board)
-
-            # Choose a position
-            position = player_choice(the_board)
-            # Place the marker on the position
-            place_marker(the_board, player1_marker, position)
-            # Check if they won
-            if win_check(the_board, player1_marker):
-                print('\n' * 100)
-                display_board(the_board)
-                print('Player 1 has won')
-                game_on = False
-            else:
-                # Or check if there is a tie
-                if full_board_check(the_board):
-                    display_board(the_board)
-                    print('Tie game')
-                    game_on = False
-                else:
-                    # No tie and no Win? then next player's turn
-                    turn = 'Player 2'
-                    print('\n' * 100)
-
-        ### PLAYER TWO TURN
-        else:
-            # Show the board
-            display_board(the_board)
-
-            # Choose a position
-            position = player_choice(the_board)
-            # Place the marker on the position
-            place_marker(the_board, player2_marker, position)
-            # Check if they won
-            if win_check(the_board, player2_marker):
-                print('\n' * 100)
-                display_board(the_board)
-                print('Player 2 has won')
-                game_on = False
-            else:
-                # Or check if there is a tie
-                if full_board_check(the_board):
-                    display_board(the_board)
-                    print('Tie game')
-                    game_on = False
-                else:
-                    # No tie and no Win? then next player's turn
-                    turn = 'Player 1'
-                    print('\n' * 100)
-
-    # BREAK OUT OF THE WHILE LOOP ON replay()
-    if not replay():
-        break
+            print("Player 2 TURN")
+            player_two_bot()
+            print("Player 1 TURN")
+            player_one_move()
+    if turn == "Player 1":
+        print("Player 1 TURN")
+        player_one_move()
+        print("Player 2 TURN")
+        player_two_move()
+    else:
+        print("Player 2 TURN")
+        player_two_move()
+        print("Player 1 TURN")
+        player_one_move()
